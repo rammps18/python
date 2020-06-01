a021cf036767a80bdb2fead93c7811fb2bf079d1
rammps18 <rammps18@users.noreply.github.com>
2020-06-01 05:57:09 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,3 +2,10 @@
 divide = lambda x,y : x/y
 
 print(divide(15,3))
+
+def over_age(data, getter):
+    return getter(data) >= 18
+ 
+user = { 'username': 'rolf123', 'age': '35' }
+ 
+print(over_age(user, lambda x: int(x['age'])))
